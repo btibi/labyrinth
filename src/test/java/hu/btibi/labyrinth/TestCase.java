b5f5c3f8e335d98866514a82bb63a0e2672736dc
btibi <btibi@vipmail.hu>
2013-05-30 18:53:06 +0200

    junit

---
+++ src/test/java/hu/btibi/labyrinth/TestCase.java
@@ -0,0 +1,13 @@
+package hu.btibi.labyrinth;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TestCase {
+
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+}
